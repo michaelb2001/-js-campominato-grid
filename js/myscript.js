080f6351bc87491a4869afc8857075aaafa4b161
michaelb2001 <brunco.michael@gmail.com>
2021-12-20 17:32:36 +0100

    seleziona celle

--- js/myscript.js
+++ js/myscript.js
@@ -4,80 +4,57 @@ document.getElementById("play").addEventListener("click",function(){
     //creo la funziona che stabilizza il livello e crea la tabella di gioco di conseguenza{
         const lv = document.getElementById("modalita").value;
         let tabella = document.getElementById("tabella");
-    
+        let colPerRow = 10;
         //controllo difficoltà e creazione tabella di conseguenza
-    
-        if( lv == "facile" ) {
-            tabella.innerHTML="";
-          for(let i=0;i < 10;i++){
-            tabella.innerHTML += `
-            <div id="riga-${i}" class="d-flex flex-row justify-content-center">
-            </div>
-            `;
-    
-            for(let j=0;j < 10;j++){
-                document.getElementById("riga-"+i).innerHTML += 
-                `
-                <div class="col-1 blocco "> 
-                </div>
-                `;
-            }
-    
-          }  
+        tabella.innerHTML = "";
+        if( lv == "facile" ) {            
+            colPerRow = 10;            
         };
     
-        if( lv == "media" ) {
-            tabella.innerHTML="";
-            for(let i=0; i < 9 ;i++){
-                tabella.innerHTML += `
-                <div id="riga-${i}" class="d-flex flex-row justify-content-center">
-                </div>
-                `;
-        
-                for(let j=0; j < 9 ;j++){
-                    document.getElementById("riga-"+i).innerHTML += 
-                    `
-                    <div class="col-1 blocco "> 
-                    </div>
-                    `;
-                };
-        
-            }; 
+        if( lv == "media" ) {           
+            colPerRow = 9;
         };
 
-        if( lv == "difficile" ) {
-            tabella.innerHTML="";
-            for(let i=0; i < 7 ;i++){
-                tabella.innerHTML += `
-                <div id="riga-${i}" class="d-flex flex-row justify-content-center">
-                </div>
-                `;
-        
-                for(let j=0; j < 7 ;j++){
-                    document.getElementById("riga-"+i).innerHTML += 
-                    `
-                    <div class="col-1 blocco"> 
-                    </div>
-                    `;
-                };
-        
-            };  
+        if( lv == "difficile" ) {           
+            colPerRow = 7;           
         };
+
+        generaCelle( colPerRow )  
 
         let myArray = document.getElementsByClassName("blocco");
-console.log(myArray);
-for (let index = 0; index < myArray.length; index++) {
 
-    myArray[index].addEventListener("click",function(){
-        this.classList.remove("blocco");
-        this.classList.add("acqua");
-
-    });
-
-};
+        selezionaCelle(myArray);
 
 });
 
+function generaCelle( numeroCelle )
+{
+    let contatore = 1;
+    for(let i=0;i < numeroCelle; i++){
 
+        tabella.innerHTML += `<div id="riga-${i}" class="d-flex flex-row justify-content-center"></div>`;
+        const riga         = document.getElementById( "riga-" +i )
+        
+        for(let j=0;j < numeroCelle; j++){
 
+                riga.innerHTML += 
+                `<div id="cella-${i}-${j}" class="col-1 blocco ">${ ( numeroCelle * i ) + j +1 } 
+                </div> `;
 
+                contatore++
+        } 
+        
+    } 
+}
+
+function selezionaCelle(myArray){
+    for (let index = 0; index < myArray.length; index++) {
+
+        myArray[index].addEventListener("click",function(){
+            this.classList.remove("blocco");
+            this.classList.add("acqua");
+    
+        });
+    }   
+
+};
